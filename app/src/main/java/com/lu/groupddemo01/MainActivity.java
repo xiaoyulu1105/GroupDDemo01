a4898472e6d4d66657ac8c3fd8dcd6f3cc4032ba
卢小玉 <923121067@qq.com>
2016-11-19 14:15:12 +0800

    创建项目

---
+++ app/src/main/java/com/lu/groupddemo01/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lu.groupddemo01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 文件刚刚创建
+    }
+}
